aa72dc5f67335cef49fe539fa7a65d6ef139105a
i8284 <109680222+i8284@users.noreply.github.com>
2025-03-23 23:17:21 +0300

    Update CustomsService.java

--- hm/src/ru/netology/service/CustomsService.java
+++ hm/src/ru/netology/service/CustomsService.java
@@ -1,7 +1,9 @@
 package ru.netology.service;
 
 public class CustomsService {
-    public static final int DUTY_RATE() {
-        return 100;
+    public static final int DUTY_RATE = 100;
+
+    public static int calculateCustoms(int a, int b) {
+        return (a/100) + b * DUTY_RATE;
     }
 }
